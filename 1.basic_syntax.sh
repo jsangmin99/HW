c8c3f3ec31b1ab571ad08aa769ddbdb1f861eeb0
jeonsangmin <jsangmin99@gmail.com>
2024-05-14 17:48:00 +0900

    2days

--- 1.basic_syntax.sh
+++ 1.basic_syntax.sh
@@ -42,6 +42,9 @@ touch firtst_file.txt
 # 파일 내용 조회
 cat firt_file.txt
 
+# 파일 내용조회(터미널크기에 따라 페이지 단위로)
+more first_file.txt
+
 # 터미널창에 문자열을 풀력하는 명령어
 echo "hello world"
 
@@ -86,3 +89,26 @@ nano 파일명
 
 # nano 명령어는 운영체제의 배포판 마다 다르다. 하지만 vi는 대부분 설치되어있다.
 
+
+# find는 파일명으로 검색 | grep 문자열 찾기
+
+# 사용자 추가
+useradd 사용자명
+
+# 패스워드 지정
+passwd 사용자명
+# 사용자 목록 조회
+cat /etc/passwd
+
+#사용자 변환
+su - 사용자명
+
+# 파일 권한 관리
+# d|rwx|rwx|rwx
+# (디렉토리인지 아닌지)| user | group | others 
+# 각 권한은 r(4)w(2)x(1) 이루어져있음
+chmod 777 test.sh
+
+# 파일 소유자.그룹 관리
+# 소유자와 그룹 변경
+chown 소유자: 그룹:파일명
